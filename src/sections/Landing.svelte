c98a75f2c7bab849798141702ca322d15c868b51
Struck713 <noahstruck71303@gmail.com>
2025-04-13 18:43:25 -0400

    Migrate to Luxon (moment is deprecated)

--- src/sections/Landing.svelte
+++ src/sections/Landing.svelte
@@ -1,12 +1,12 @@
 
 <script lang="ts">
-    import moment from "moment";
-    import Button from "../components/Button.svelte";
     import { goto } from "$app/navigation";
-    const returnDate = moment("02/15/2025").fromNow(true);
+    import { DateTime } from "luxon";
+    import Button from "../components/Button.svelte";
+    const returnDate = DateTime.fromFormat("02/15/2025", "dd/mm/yyyy").toRelativeCalendar();
 </script>
 
 <div class="h-[80vh] mx-[22vw] flex flex-col justify-center items-center gap-3">
-    <h1 class="text-3xl">Kent Hack Enough will return <span class="font-bold">in {returnDate}</span>...</h1>
+    <h1 class="text-3xl">Kent Hack Enough will return <span class="font-bold">{returnDate}</span>...</h1>
     <Button on:click={() => goto("/apply")}> But you can apply now 🚀 </Button>
 </div>
