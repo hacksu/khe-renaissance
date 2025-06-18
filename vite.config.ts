3e0521ec86867297bd47c9f1fc3d7a94448fd900
Struck713 <noahstruck71303@gmail.com>
2025-06-17 23:01:44 -0400

    Changed configurations and added port to loadbalancer on service for Traefik

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
-		allowedHosts: ["localhost.khe.io", "khe.io"]
+		allowedHosts: true
 	}
 });
