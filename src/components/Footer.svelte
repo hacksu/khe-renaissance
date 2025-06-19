65622cdbe043e549578d8db65b6dd6d24d8ff58f
Austin Sternberg <austinplaysmms12@gmail.com>
2025-06-19 14:26:59 -0400

    UI improvements

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,11 +1,14 @@
-
 <script lang="ts">
   import Logo from "../assets/hacksu.svg";
+  import MLHLogo from "../assets/mlh.svg";
 </script>
 
-<div class="bg-secondary py-2 px-10 mt-24 flex flex-row text-white">
+<div class="bg-secondary py-2 px-10 mt-24 flex flex-row text-white gap-2">
     <p class="self-center font-bold">POWERED BY</p>
     <a href="https://hacksu.com" target="_blank">
       <img src={Logo} alt="HacKSU Logo" class="w-24 transition-opacity duration-300 hover:opacity-75" />
     </a>
+    <!--<a href="https://mlh.io" target="_blank" class="self-center">
+      <img src={MLHLogo} alt="MLH Logo" class="w-24 transition-opacity duration-300 hover:opacity-75" />
+    </a>  -->
 </div>
--- src/sections/Sponsors.svelte
+++ src/sections/Sponsors.svelte
@@ -6,16 +6,23 @@
     image: any;
   }
 
-  import Logo from "../assets/sponsors/bawls.webp";
+  import BawlsLogo from "../assets/sponsors/bawls.webp";
+  import KSULogo from "../assets/sponsors/ksu-logo.svg";
+  import DepartLogo from "../assets/sponsors/department.png";
 
   const sponsors: Sponsor[] = [
     {
-      image: Logo,
+      image: KSULogo,
       name: "Kent State University",
       url: "https://cs.kent.edu"
     },
     {
-      image: Logo,
+      image: DepartLogo,
+      name: "Department of Computer Science",
+      url: "https://cs.kent.edu"
+    },
+    {
+      image: BawlsLogo,
       name: "Enbasis",
       url: "https://enbasis.com"
     }
@@ -25,7 +32,11 @@
 <div id="sponsors" class="flex flex-col lg:flex-row gap-4 items-center justify-center">
     {#each sponsors as { image, name, url}}
         <a class="w-96 lg:w-56" href={url} target="_blank">
-            <img class="object-cover rounded-lg" src={image} alt={name} />
+            <img
+                class="rounded-lg w-[300px] h-[100px] object-contain transition-transform hover:scale-105"
+                src={image}
+                alt={name}
+            />
         </a>
     {/each}
 </div>
