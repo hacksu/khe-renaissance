b21c843b642c14c2f0190885025f4727d034d57b
Austin Sternberg <austinplaysmms12@gmail.com>
2025-06-19 12:42:54 -0400

    minor tweaks/updates

--- src/sections/Landing.svelte
+++ src/sections/Landing.svelte
@@ -3,7 +3,8 @@
     import { goto } from "$app/navigation";
     import { DateTime } from "luxon";
     import Button from "../components/Button.svelte";
-    const returnDate = DateTime.fromFormat("03/15/2026", "dd/mm/yyyy").toRelativeCalendar();
+    // const returnDate = DateTime.fromFormat("03/28/2026", "dd/mm/yyyy").toRelativeCalendar();
+    const returnDate = "in March";
 </script>
 
 <div class="bg-[url(/background.jpg)] bg-cover bg-center h-screen flex flex-col justify-center items-center gap-3 text-center">
