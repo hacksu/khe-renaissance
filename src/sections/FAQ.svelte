778cce6c0962a59d6fef96b003b00fbb3c13956a
Austin Sternberg <austinplaysmms12@gmail.com>
2025-06-26 20:23:25 -0400

    MLH Requirements

--- src/sections/FAQ.svelte
+++ src/sections/FAQ.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-    import Link from "$components/Link.svelte";
+    import Input from "$components/form/Input.svelte";
+import Link from "$components/Link.svelte";
     import Accordion from "../components/Accordion.svelte";
     import Card from "../components/Card.svelte";
 
@@ -17,13 +18,17 @@
                     <br />
                     <p>Attendees meet on Saturday, make a team, and have 24 hours to build a tech-related project. There will be optional lessons and talks, sponsors looking to hire smart, motivated people, and tons of free stuff!</p>
                     <br />
+                    <p>Then on Sunday, teams will present their projects to a panel of judges.</p>
                     <p>We even have a nicely written guide for this, you can <Link href="/guide" target="_self">view it here</Link>!</p>
+                    <br />
+                    <br />
+                    <p><i>Note: This is a Major League Hacking (MLH) event, which means that it and all attendees are subject to the <Link href="https://mlh.io/code-of-conduct" target="_blank">MLH Code of Conduct</Link>.</i></p>
                 {/snippet}
             </Accordion>
             <Accordion>
                 {#snippet name()}Can I participate?{/snippet}
                 {#snippet content()}
-                    <p>If you're a college student of any kind, then yes! KHE is attended by people of all skill levels and backgrounds. You'll fit right in and hopefully, learn something!</p>
+                    <p>If you're a college student, working professional, or anyone over 18, then yes! KHE is attended by people of all skill levels and backgrounds. You'll fit right in and hopefully, learn something!</p>
                 {/snippet}
             </Accordion>
             <Accordion>
