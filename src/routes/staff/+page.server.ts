8cb461b4a0c3af98ba082315e2a879f8e2b0b1ea
Struck713 <noahstruck71303@gmail.com>
2025-09-06 01:35:47 -0400

    Add ability to approve and check in applications

--- src/routes/staff/+page.server.ts
+++ src/routes/staff/+page.server.ts
@@ -1,7 +1,37 @@
 import { prisma } from "$lib/server/prisma";
-import type { PageServerLoad } from "./$types";
+import type { Actions, PageServerLoad } from "./$types";
 
-export const load: PageServerLoad = async ({ request, parent }) => {
+export const actions: Actions = {
+    approve: async ({ request }) => {
+        const form = await request.formData();
+        const id = form.get("id") as string;
+
+        if (!id) {
+            return;
+        }
+
+        const application = await prisma.application.findFirst({ where: { id }});
+        await prisma.application.update({
+            where: { id },
+            data: { approved: !application?.approved }
+        });
+    },
+    checkIn: async ({ request }) => {
+        const form = await request.formData();
+        const id = form.get("id") as string;
+
+        if (!id) {
+            return;
+        }
+
+        await prisma.application.update({
+            where: { id },
+            data: { checkedIn: true }
+        });
+    }
+};
+
+export const load: PageServerLoad = async () => {
     const applications = await prisma.application.findMany({
         include: {
             user: true
