11beb2069bfd6ad1423401ad761b8596ca8af462
Monster0506 <tjraklovits@gmail.com>
2025-11-28 13:34:27 -0500

    Add link to schedule page from landing

--- src/sections/Landing.svelte
+++ src/sections/Landing.svelte
@@ -8,7 +8,8 @@
 
 <div class="bg-[url(/background.jpg)] bg-cover bg-center h-screen flex flex-col justify-center items-center gap-3 text-center">
     <h1 class="text-4xl">Kent Hack Enough will return <span class="font-bold">{returnDate}</span>...</h1>
-    <div class="w-96">
-        <Button size="lg" onclick={() => goto("/auth/login")}>In the meantime, Apply now!</Button>
+    <div class="flex flex-col md:flex-row gap-3 w-96">
+        <Button size="lg" onclick={() => goto("/auth/login")}>Apply now!</Button>
+        <Button size="lg" onclick={() => goto("/schedule")}>View Schedule</Button>
     </div>
 </div>
