a5638d49030fe6c13a6bf34433085aa761bd7645
Austin Sternberg <austinplaysmms12@gmail.com>
2025-12-02 11:28:48 -0500

    Revert "Merge pull request #52 from hacksu/dev"
    This reverts commit 6d6b2dabf52d36e8098001b9e24970e030b7f035, reversing
    changes made to cd6fcd0dddfbeaed6fc7af2f3e88dcad162e66d1.

--- src/lib/server/email.ts
+++ src/lib/server/email.ts
@@ -1,36 +1,6 @@
 import { env } from "$env/dynamic/private";
 import { google } from "googleapis";
 
-const APPROVAL_EMAIL_TEXT = 
-`Hi there,
-
-Great news! Your application to Kent Hack Enough 2026 has been approved.
-
-The event will take place on March 28–29th. More updates will be shared soon, so stay tuned.
-
-In the meantime, be sure to join our community Discord:
-https://discord.gg/FHrw9AHtA8
-
-And remember to sign up on our Devpost page so you will be able to submit your project during the hackathon:
-https://kent-hack-enough-2026.devpost.com/
-
-If you have any questions at all, feel free to reach out. We are here to help.
-
-Thanks,
-Kent Hack Enough 2026 Team`;
-
-const APPROVAL_EMAIL_HTML = 
-`<p>Hi there,</p>
-<p>Great news! Your application to <strong>Kent Hack Enough 2026</strong> has been approved.</p>
-<p>The event will take place on <strong>March 28–29th</strong>. More updates will be shared soon, so stay tuned.</p>
-<p>In the meantime, be sure to join our community Discord:<br>
-<a href="https://discord.gg/FHrw9AHtA8">https://discord.gg/FHrw9AHtA8</a></p>
-<p>And remember to sign up on our Devpost page so you will be able to submit your project during the hackathon:<br>
-<a href="https://kent-hack-enough-2026.devpost.com/">https://kent-hack-enough-2026.devpost.com/</a></p>
-<p>If you have any questions at all, feel free to reach out. We are here to help.</p>
-<p>Thanks,<br>
-<strong>Kent Hack Enough 2026 Team</strong></p>`;
-
 // Gmail API client for OAuth2
 const oauth2Client = new google.auth.OAuth2(
     env.GMAIL_CLIENT_ID,
@@ -74,8 +44,8 @@ const createEmailMessage = (from: string, to: string, subject: string, text: str
 export const sendApprovalEmail = async (to: string) => {
     const from = env.GMAIL_FROM || env.GMAIL_USER || "staff@khe.io";
     const subject = "Your application to KHE has been approved!";
-    const text = APPROVAL_EMAIL_TEXT;
-    const html = APPROVAL_EMAIL_HTML;
+    const text = "Congratulations! Your application to Kent Hack Enough has been approved. We look forward to seeing you there!";
+    const html = "<p>Congratulations! Your application to <strong>Kent Hack Enough</strong> has been approved. We look forward to seeing you there!</p>";
     
     try {
         const message = createEmailMessage(from, to, subject, text, html);
@@ -99,8 +69,8 @@ export const sendApprovalEmail = async (to: string) => {
 export const sendApprovalRevokedEmail = async (to: string) => {
     const from = env.GMAIL_FROM || env.GMAIL_USER || "staff@khe.io";
     const subject = "Your KHE application approval has been revoked";
-    const text = REVOKED_EMAIL_TEXT;
-    const html = REVOKED_EMAIL_HTML;
+    const text = "Your application to Kent Hack Enough has been updated, and your approval status has been revoked. Please re-submit your application if you wish to be considered again.";
+    const html = "<p>Your application to <strong>Kent Hack Enough</strong> has been updated, and your approval status has been revoked. Please re-submit your application if you wish to be considered again.</p>";
     
     try {
         const message = createEmailMessage(from, to, subject, text, html);
