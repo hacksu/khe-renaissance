faad8cf9a6637a019314ca59a959636067b02661
Monster0506 <tjraklovits@gmail.com>
2026-01-15 15:39:30 -0500

    Better add components alias to vite?

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
 	plugins: [sveltekit()],
 	resolve: {
 		alias: {
-			$components: '/src/components',
+			$components: path.resolve(__dirname, './src/components'),
 		}
 	},
 	server: {
